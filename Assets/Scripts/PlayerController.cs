9842ce41bc623e021ee374ceb150ce9e55c32ad4
Echo Yin <echo_ybl@hotmail.com>
2023-07-19 18:50:16 -0400

    Add player control scheme and movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,31 +6,54 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    public float movementSpeed = 1.0f;
+    public float jumpHeight = 1.0f;
+    
     private Rigidbody _rigidbody = null;
-
-    private float _movmentX = 0.0f;
-
+    private float _movementX = 0.0f;
+    private float _movementZ = 0.0f;
     private float _movementY = 0.0f;
+
+    private bool _isGrounded = true;
+    
     // Start is called before the first frame update
     void Awake()
     {
         _rigidbody = GetComponent<Rigidbody>();
     }
     
-    // 
+    // Get the movement data
     void OnMove(InputValue movementValue)
     {
         var movementVector = movementValue.Get<Vector2>();
-        _movmentX = movementVector.x;
-        _movementY = movementVector.y;
+        _movementX = movementVector.x;
+        _movementZ = movementVector.y;
         // Debug.Log(message: string.Format("{0} {1}", _movmentX, _movementY));
     }
+
+    // void OnJump(InputValue jumpValue)
+    // {
+    //     var jumpVector = jumpValue.Get<Vector3>();
+    //     _movementY = jumpVector.z;
+    // }
     
-    // Update is called once per frame
+    // Update is called once per frame, all physics calculations are better here
     private void FixedUpdate()
     {
-        var movementVector3 = new Vector3(_movmentX, 0.0f, _movementY);
-        _rigidbody.AddForce(movementVector3);
+        MoveBall();
+        // JumpBall();
     }
+
+    void MoveBall()
+    {
+        var movementVector3 = new Vector3(_movementX, 0.0f, _movementZ);
+        _rigidbody.AddForce(movementVector3 * movementSpeed);
+    }
+
+    // void JumpBall()
+    // {
+    //     var movementVector3 = new Vector3(0.0f, _movementY, 0.0f);
+    //     _rigidbody.AddForce(movementVector3 * jumpHeight);
+    // }
  
 }
